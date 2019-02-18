1971340b1a71164eb5950b274c473e378b1094c4
João Ferreira <joao12ferreira@gmail.com>
2019-02-18 23:36:33 +0000

    Add files via upload

---
+++ SharePointApplicationInsights/Features/Feature1/Feature1.feature
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="721db3d5-d28f-4b15-a69d-d853f8588531" description="Enable Application Insights without modifying the master page" featureId="721db3d5-d28f-4b15-a69d-d853f8588531" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint Application Insights" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c0b61edc-ca7a-4968-96cd-83ffe16a4514" />
+    <projectItemReference itemId="d307779f-b4f5-4a0a-8599-6f47dc0ad1df" />
+  </projectItems>
+</feature>
---
+++ SharePointApplicationInsights/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharePointApplicationInsights")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharePointApplicationInsights")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8acd92c3-b3a3-44f1-9b31-af26739f49e5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
